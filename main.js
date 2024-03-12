2ccff451c9b96b920123418379dca1047859d72d
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2024-03-12 18:31:48 +0300

    both web-app and desktop app works together

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 // This file is created for antry point of Electron
+const {app, BrowserWindow} = require('electron');
+
+// Creation of the mainWindow with specified WxH 
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+
+    // to load window in specified url
+    mainWindow.loadURL(`http://${process.env.HOST || 'localhost'}:${process.env.PORT || 3000}`);
+}
+
+// to lunch application
+app.on('ready',() => {
+    // TODO CREATE MENU
+
+    // Create window
+    createWindow();
+});
