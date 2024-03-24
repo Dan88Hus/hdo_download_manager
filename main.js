f8b18e8e4da7d2854aee304d4af713d278acdd8f
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2024-03-24 17:35:52 +0300

    reportHistory.html before JSscript

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 // This file is created for antry point of Electron
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, Menu, Tray} = require('electron');
 const url = require('url');
 const path = require('path');
+const template = require('./menu/Menu');
+const iconPng = path.join(__dirname,'public','favIcoPng256x256.png');
+const iconFav = path.join(__dirname,'public','favicon.ico');
+
 
 
 // Creation of the mainWindow with specified WxH 
@@ -27,9 +31,17 @@ function createWindow() {
 }
 
 
-// to lunch application
+// to lunch application with Menu
 app.on('ready',() => {
-    // TODO CREATE MENU
+    // Create Menu
+    const menuTray = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menuTray);
+    
+    if(process.platform === 'win32'){
+      tray = new Tray(iconFav);
+      tray.setContextMenu(menuTray);
+    }
+
 
     // Create window
     createWindow();
---
+++ menu/Menu.js
@@ -0,0 +1,30 @@
+const { dialog, app, BrowserWindow } = require('electron');
+
+// Creation of the mainWindow with specified WxH 
+function reportHistoryWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 600,
+        height: 400,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+
+    // to load window in specified file instead of webPage
+    mainWindow.loadFile('reportHistory.html');
+}
+
+const template = [
+  {
+    label: 'Menu',
+    submenu: [{label: 'Exit', click: async () => {app.quit();},},],
+  },
+  {
+    label: 'Reports',
+    submenu: [{label: 'Report History', click: async () => { 
+        await app.whenReady().then(()=>{reportHistoryWindow()})
+    },},],
+  },
+];
+
+module.exports = template;
